5d092ff1c1b3e5f40802544c78e17daf50bfc42e
Kent Chen <chkche1@gmail.com>
2018-01-02 22:41:58 -0800

    Interesting implementation of largest-series-product

--- largest-series-product/src/main/kotlin/Series.kt
+++ largest-series-product/src/main/kotlin/Series.kt
@@ -1,5 +1,4 @@
 class Series(private val s: String) {
-
     init {
         require(s.all { it.isDigit() }) { "input contains non-digit character"}
     }
@@ -26,4 +25,26 @@ class Series(private val s: String) {
     private fun charToDigit(c: Char): Int {
         return c - '0'
     }
+
+    // Interesting implementation - http://exercism.io/submissions/5a6f7f10e5dd42ad82dc7dd6396fd3f4
+
+    /*
+	init {
+		require (s.all { it.isDigit()})
+	}
+
+	fun getLargestProduct(n : Int) : Int {
+		require (n <= s.length && n >= 0)
+		return if (n == 0) 1
+			    else
+				s.asIterable().windowed(size = n) {
+					window : List<Char> -> window.fold(1, {product, element-> product*decimalDigitValue(element)})
+				}.max()?: 1
+	}
+
+	fun decimalDigitValue(c: Char): Int {
+        return c.toInt() - '0'.toInt() // Explicit conversions to numbers
+	}
+    */
+
 }
