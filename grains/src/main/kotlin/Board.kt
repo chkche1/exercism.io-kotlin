256ec9f1a8b4bd9e90a4a2d8d745344f8ed2eb70
Kent Chen <chkche1@gmail.com>
2018-01-03 21:53:38 -0800

    Use BigInteger.ONE for consistancy

--- grains/src/main/kotlin/Board.kt
+++ grains/src/main/kotlin/Board.kt
@@ -4,7 +4,7 @@ object Board {
 
     fun getGrainCountForSquare(n: Int): BigInteger {
         require (n in 1..64) { "Only integers between 1 and 64 (inclusive) are allowed" }
-        return if (n == 1) BigInteger.ONE else BigInteger("1").shiftLeft(n - 1)
+        return if (n == 1) BigInteger.ONE else BigInteger.ONE.shiftLeft(n - 1)
     }
 
     fun getTotalGrainCount(): BigInteger {
