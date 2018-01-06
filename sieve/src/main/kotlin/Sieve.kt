fe8f5f7fb6eb57711c846472f434c084744e6a0d
Kent Chen <chkche1@gmail.com>
2018-01-05 23:27:30 -0800

    Adding an interesting more functional impl for sieve

--- sieve/src/main/kotlin/Sieve.kt
+++ sieve/src/main/kotlin/Sieve.kt
@@ -12,4 +12,14 @@ object Sieve {
         primeTable.forEachIndexed({ idx, b -> if (b) primes.add(idx) })
         return primes
     }
+
+    /*
+    Reference impl: http://exercism.io/submissions/d66bdbf7fe49486a83afa765ff3969a0
+
+    fun primesUpTo(limit: Int) : List<Int> {
+        val range = (2..limit)
+        return range.fold((range.toList())) { acc, i ->
+            acc.minus((2*i)..range.last() step i) }
+    }
+    */
 }
