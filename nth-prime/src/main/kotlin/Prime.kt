bda74664f2bdd0a643d9f83e6b1e9257b7be3f4a
Kent Chen <chkche1@gmail.com>
2018-01-11 23:46:07 -0800

    nth-prime

---
+++ nth-prime/src/main/kotlin/Prime.kt
@@ -0,0 +1,44 @@
+object Prime {
+
+    fun nth(n: Int): Int {
+        require(n > 0) { "There is no zeroth prime." }
+
+        if (n == 1) {
+            return 2
+        } else if (n == 2) {
+            return 3
+        }
+
+        var current = 3
+        var count = 2
+        while (count < n) {
+            do {
+                current += 2
+            } while (!isPrime(current))
+
+            count++
+        }
+
+        return current
+    }
+
+    private fun isPrime(n: Int): Boolean {
+        if (n == 2 || n == 3) {
+            return true
+        }
+
+        if (n == 1 || (n.and(1)) == 0) {
+            return false
+        }
+
+        var end = Math.sqrt(n.toDouble()).toInt()
+        for (i in 3..end step 2) {
+            if (n % i == 0) {
+                return false
+            }
+        }
+
+        return true
+    }
+
+}
