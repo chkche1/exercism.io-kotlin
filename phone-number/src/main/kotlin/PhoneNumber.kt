a24eacf5535679c4c4270813cc61bdc366fa0e77
Kent Chen <chkche1@gmail.com>
2018-01-16 22:35:06 -0800

    A functional solution to phone-number

--- phone-number/src/main/kotlin/PhoneNumber.kt
+++ phone-number/src/main/kotlin/PhoneNumber.kt
@@ -1,5 +1,20 @@
+/**
+ * Interesting impl - http://exercism.io/submissions/3e2d9f368af64dfaa01cb0b06eb22aa9
+ *
+ * Below is a solution based on idea above. The use of `let`, `?.`, and takeIf is neat
+ */
 class PhoneNumber(private val raw: String) {
 
+    var number : String? = raw.filter { it.isDigit() }
+            .let {
+                when (it.length) {
+                    11 -> if (it[0]=='1') it.drop(1) else null
+                    10 -> it
+                    else -> null
+                }
+            }?.takeIf { ('2'..'9').contains(it[0]) && ('2'..'9').contains(it[3]) }
+
+/*
     val number by lazy(mode = LazyThreadSafetyMode.NONE) {
         convert()
     }
@@ -16,15 +31,13 @@ class PhoneNumber(private val raw: String) {
         return null
     }
 
-    /**
-     * Expects input to be a 10-digit string
-     */
+    //Expects input to be a 10-digit string
     private fun validate(input: String): String? {
         if (input[0] !in '2'..'9' || input[3] !in '2'..'9') {
             return null
         }
 
         return input
-    }
+    }*/
 
 }
