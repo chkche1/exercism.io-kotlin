00b5df6a887a4441700ab526cb809d163dd82ae1
Kent Chen <chkche1@gmail.com>
2018-02-10 03:14:19 -0800

    allergies

---
+++ allergies/src/main/kotlin/Allergies.kt
@@ -0,0 +1,11 @@
+class Allergies(score: Int) {
+    private val allergies = mutableListOf<Allergen>()
+
+    init {
+        Allergen.values().filterTo(allergies) { it.score and score > 0 }
+    }
+
+    fun isAllergicTo(allergen: Allergen) = allergies.contains(allergen)
+
+    fun getList(): List<Allergen> = allergies.toList()
+}
