b7dbf856cd030de0e2a7d151921506ae33296e75
Kent Chen <chkche1@gmail.com>
2018-02-22 11:48:56 +0530

    Improved series to be more functional and concise

--- series/src/main/kotlin/Series.kt
+++ series/src/main/kotlin/Series.kt
@@ -1,8 +1,10 @@
 object Series {
 
-    fun slices(n: Int, s: String): List<List<Int>> {
-        return if (n > s.length) emptyList()
+    /*Improved implementation based on http://exercism.io/submissions/929533e1004d4d8ca553ad9e2da921fb */
+    fun slices(n: Int, s: String): List<List<Int>> = s.map { Character.getNumericValue(it) }.windowed(n)
+
+    /*
+    fun slices(n: Int, s: String): List<List<Int>> = if (n > s.length) emptyList()
         else (0..s.length - n).map { i -> (0 until n).map { Integer.parseInt(s.substring(i+ it, i+ it +1)) } }
-    }
-    
+    */
 }
