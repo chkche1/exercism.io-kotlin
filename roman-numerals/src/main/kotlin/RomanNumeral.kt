d67e623e56aa9c135ece6502db8fd289fa1ef87d
Kent Chen <chkche1@gmail.com>
2018-03-19 20:43:16 -0700

    roman-numerals

--- roman-numerals/src/main/kotlin/RomanNumeral.kt
+++ roman-numerals/src/main/kotlin/RomanNumeral.kt
@@ -1,7 +1,27 @@
 object RomanNumeral {
 
-    fun value(s: String): String {
+    private val map = mapOf( 1000 to "M",
+            900 to "CM",
+            500 to "D",
+            400 to "CD",
+            100 to "C",
+            90 to "XC",
+            50 to "L",
+            40 to "XL",
+            10 to "X",
+            9 to "IX",
+            5 to "V",
+            4 to "IV",
+            1 to "I")
 
+    fun value(s: Int) = buildString {
+        var num = s
+        map.forEach { k, v ->
+            while (num >= k) {
+                num -= k
+                append(v)
+            }
+        }
     }
 
 }
