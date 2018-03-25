e44f4b19614162b6fa1fed29aeac0b2f0ad44ef3
Kent Chen <chkche1@gmail.com>
2018-03-24 23:36:59 -0700

    Use flatMap

--- etl/src/main/kotlin/ETL.kt
+++ etl/src/main/kotlin/ETL.kt
@@ -1,9 +1,6 @@
 object ETL {
 
-    fun transform(map: Map<Int, List<Char>>): Map<Char, Int> {
-        val res = mutableMapOf<Char, Int>()
-        map.forEach {entry -> entry.value.forEach { res[it.toLowerCase()] = entry.key } }
-        return res
-    }
+    fun transform(map: Map<Int, List<Char>>) =
+            map.flatMap { (k, v) -> v.map { it.toLowerCase() to k } }.toMap()
 
 }
